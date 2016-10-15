9396bb4f2f05e708c97e4959f250277c256c35b3
Vicheanak <vicheanak@gmail.com>
2016-10-15 19:57:24 +0700

    add apk

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ var cors = require('cors');
 var index = require('../../app/controllers/index');
 
 module.exports = function(app) {
-	app.options('/', cors());
-	app.route('/')
-	.get(cors(), index.render);
+	// app.options('/', cors());
+	// app.route('/')
+	// .get(cors(), index.render);
 };
 
 
